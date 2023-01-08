0c60171352d36ab83108de14d8f5d44519eb95b9
Gabriel Muniz <119897742+gabrielficto@users.noreply.github.com>
2023-01-08 19:58:45 -0300

    funções print e randomInt

---
+++ compile.sh
@@ -0,0 +1,1 @@
+tsc -watch fictojs.ts
--- fictojs.ts
+++ fictojs.ts
@@ -1,7 +1,38 @@
-class FictoJS {
-    static exec(source: string){
+const keywords = {
+    "fun ": "function "
+};
 
+const errors = {
+    MAIN_FUNC_NOT_FOUND: "main function not found"
+}
+
+class Ficto {
+    static print(arg: any): void {
+        document.write(arg);
+    }
+
+    static randomInt(min: number, max: number): number {
+        return Math.floor(Math.round(Math.random() * (max - min)) + min);
     }
 }
 
-FictoJS.exec(document.querySelector('ficto')?.innerText);
+class FictoJS {
+    static exec(source: string){
+        if(!source.includes("main"))
+            throw(errors.MAIN_FUNC_NOT_FOUND); 
+
+        eval(source.replace("fun ", "function "));
+    }
+
+    static cleanUp(){
+        (document.querySelector('ficto') as HTMLDivElement).innerText = "";
+    }
+}
+
+try {
+    FictoJS.exec((document.querySelector('ficto') as HTMLDivElement)?.innerText);
+} catch(err){
+    document.write(`<h5 style="color: red">Error: ${err}<h5>`);
+}
+
+FictoJS.cleanUp();
